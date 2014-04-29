ab6895e5812a6bf904104b12a5de92e6ac869cb9
root <root@ip-10-234-27-248.us-west-2.compute.internal>
2014-04-29 09:44:03 +0000

    initial working automation with git, apache2, php, mysql

---
+++ cookbooks/phpapp/metadata.rb
@@ -0,0 +1,13 @@
+name             'phpapp'
+maintainer       'Chromedia, Inc.'
+maintainer_email 'admin@chromedia.com'
+license          'All rights reserved'
+description      'Installs/Configures phpapp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "build-essential"
+depends "apache2"
+depends "mysql"
+depends "php"
+depends "database"
